a391b4cd7b73abd8be00bedbe1a93031aa3e5759
moontaijin <moontaijin@naver.com>
2025-05-14 20:33:33 +0900

    init main

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Dockerized Flask!"
+
+@app.route("/generate", methods=["POST"])
+def generate():
+    data = request.get_json()
+    return jsonify({"result": f"AI 일기: {data.get('text', '')}"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
