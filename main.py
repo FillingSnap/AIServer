b2b784c29dcbc5d9bf218d3fcd71d0b76fbbb621
moontaijin <moontaijin@naver.com>
2025-05-16 21:58:16 +0900

    자동 배포 실험

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def home():
 @app.route("/generate", methods=["POST"])
 def generate():
     data = request.get_json()
-    return jsonify({"result": f"AI 일기: {data.get('text', '')}"})
+    return jsonify({"result": f"AI 일기 test: {data.get('text', '')}"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
