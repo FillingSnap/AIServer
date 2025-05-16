164af9478ca721aaf007ccb677b6f00e786d82d5
moontaijin <moontaijin@naver.com>
2025-05-16 22:14:04 +0900

    stream 실험

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
+import time
 
 app = Flask(__name__)
 
@@ -10,6 +11,18 @@ def home():
 def generate():
     data = request.get_json()
     return jsonify({"result": f"AI 일기 test: {data.get('text', '')}"})
+
+
+@app.route("/stream")
+def stream_by_character():
+    text = "안녕하세요 오늘 생성된 일기입니다."
+
+    def generate():
+        for ch in text:
+            yield ch
+            time.sleep(0.2)  # 글자당 지연 시간
+
+    return Response(generate(), mimetype="text/plain")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
